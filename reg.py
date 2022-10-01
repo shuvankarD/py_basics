b7441bad054ccd8b7bd28067d3c665976d39b39f
“shuvankarD” <shuvankar95@gmail.com>
2022-10-01 18:36:03 +0600

    reg

--- reg.py
+++ reg.py
@@ -1,34 +1,45 @@
+# import re
+
+# txt = "The rain in Spain"
+# x = re.search("^The.*Spain$", txt)
+# print(x)
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.search("ai", txt)
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.search(r"\bS\w+", txt)
+# print(x.string)
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.findall("in", txt)
+# print(x)
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.search("\s", txt)
+
+# print(x)
+
+# import re
+# txt = "The rain in Spain"
+# x = re.split("\s",txt)
+# print(x)
+
 import re
 
 txt = "The rain in Spain"
-x = re.search("^The.*Spain$", txt)
+x = re.split("\s", txt, 1)
 print(x)
 
-import re
-
-txt = "The rain in Spain"
-x = re.search("ai", txt)
-
-import re
-
-txt = "The rain in Spain"
-x = re.search(r"\bS\w+", txt)
-print(x.string)
-
-import re
-
-txt = "The rain in Spain"
-x = re.findall("in", txt)
-print(x)
-
-import re
-
-txt = "The rain in Spain"
-x = re.search("\s", txt)
-
-print(x)
-
-import re
-txt = "The rain in Spain"
-x = re.split("\s",txt)
+import re 
+txt ="The rain in Spain"
+x= re.sub("\s","9",txt)
 print(x)
