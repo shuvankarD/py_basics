f9028c0f64ff1df04400ef82ae4ba6c9d71d1369
“shuvankarD” <shuvankar95@gmail.com>
2022-10-01 16:15:05 +0600

    shuvosssssssssssssss

--- jss.py
+++ jss.py
@@ -12,14 +12,41 @@
 import json
 
 # a Python object (dict):
+# x = {
+#   "namee": "John",
+#   "age": 30,
+#   "city": "New York"
+# }
+
+# # convert into JSON:
+# y = json.dumps(x)
+
+# # the result is a JSON string:
+# print(y)
+
+# print(json.dumps({"name": "John", "age": 30}))
+# print(json.dumps(["apple", "bananas"]))
+# print(json.dumps(("apple", "bananas")))
+# print(json.dumps("hello"))
+# print(json.dumps(42))
+# print(json.dumps(31.76))
+# print(json.dumps(True))
+# print(json.dumps(False))
+# print(json.dumps(None))
+
+import json
+
 x = {
-  "namee": "John",
+  "name": "John",
   "age": 30,
-  "city": "New York"
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
 }
 
-# convert into JSON:
-y = json.dumps(x)
-
-# the result is a JSON string:
-print(y)
+print(json.dumps(x,indent= 5,separators=(". ", " = ")))
