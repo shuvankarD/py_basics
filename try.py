1f9d07d260caffde4a364019474238f711e1eaca
“shuvankarD” <shuvankar95@gmail.com>
2022-10-02 17:28:59 +0600

    dasssss

--- try.py
+++ try.py
@@ -9,7 +9,20 @@ try:
 except:
   print("Something went wrong when opening the file")
 
-  x = -1
+#   x = -1
 
-if x < 0:
-  raise Exception("Sorry, no numbers below zero")
+# if x < 0:
+#   raise Exception("Sorry, no numbers below zero")
+
+# x ="Hello"
+# if not type(x) is int:
+#     raise TypeError ("Pls Int only")
+
+
+try:
+  print(x)
+# except NameError:
+
+#print("Variable is not defined")
+except :
+  print("Something else is went wrong") 
