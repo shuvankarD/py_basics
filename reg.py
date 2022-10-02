155efae8f52742cee8a041cb104ac7f14880e625
“shuvankarD” <shuvankar95@gmail.com>
2022-10-02 12:59:12 +0600

    Pattern Matching

--- reg.py
+++ reg.py
@@ -33,13 +33,92 @@
 # x = re.split("\s",txt)
 # print(x)
 
+# import re
+
+# txt = "The rain in Spain"
+# x = re.split("\s", txt, 1)
+# print(x)
+
+# import re 
+# txt ="The rain in Spain"
+# x= re.sub("\s","9",txt)
+# print(x)
+
+import re
+txt = "The rain in Spain"
+x = re.findall("[a-m]",txt)
+print(x)
+
 import re
 
-txt = "The rain in Spain"
-x = re.split("\s", txt, 1)
+txt = "That will be 59 dollars"
+
+#Find all digit characters:
+
+x = re.findall("\d", txt)
 print(x)
 
-import re 
-txt ="The rain in Spain"
-x= re.sub("\s","9",txt)
+import re
+
+txt = "hello planet"
+
+#Search for a sequence that starts with "he", followed by two (any) characters, and an "o":
+
+x = re.findall("hell.", txt)
 print(x)
+
+import re
+
+txt = "hellllllllllo planet"
+
+#Check if the string starts with 'hello':
+
+x = re.findall("$ planet", txt)
+if x:
+  print("Yes, the string starts with 'hello'")
+else:
+  print("No match")
+
+
+
+#Search for a sequence that starts with "he", followed by 0 or more  (any) characters, and an "o":
+
+x = re.findall("he.*o", txt)
+
+print(x)
+
+import re
+
+txt = "hello planet"
+
+#Search for a sequence that starts with "he", followed by 1 or more  (any) characters, and an "o":
+
+x = re.findall("he.+o", txt)
+
+print(x)
+
+import re
+
+txt = "hello planet"
+
+#Search for a sequence that starts with "he", followed excactly 2 (any) characters, and an "o":
+
+x = re.findall("he.{2}o", txt)
+
+print(x)
+
+import re
+
+txt = "The rain in Spain falls mainly in the plain!"
+
+#Check if the string contains either "falls" or "stays":
+
+x = re.findall("Brazil|Django", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
