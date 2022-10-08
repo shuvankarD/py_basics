558b52c2be531a605cc309aaf89421fe7c7b8031
“shuvankarD” <shuvankar95@gmail.com>
2022-10-08 12:09:38 +0600

    my commit

--- list.py
+++ list.py
@@ -51,8 +51,56 @@
 # for x in list1:
 #     list2.append(x)
 #     print(list2)
-list1 = ["a", "b" , "c"]
-list2 = [1, 2, 3]
+# list1 = ["a", "b" , "c"]
+# list2 = [1, 2, 3]
 
-list1.extend(list2)
-print(list1)
+# list1.extend(list2)
+# print(list1)
+
+# my_list = ["mouse", [8, 4, 6], ['a']]
+# print(my_list)
+
+# my_list = ['p', 'r', 'o', 'b', 'e']
+
+# # first item
+# print(my_list[0])  # p
+
+# # third item
+# print(my_list[2])  # o
+
+# # fifth item
+# print(my_list[4])  # e
+
+# Nested List
+# n_list = ["Happy", [2, 0, 1, 5],["Shuv","nik"]]
+
+# # Nested indexing
+# print(n_list[2][1])
+
+# print(n_list[1][3])
+
+# my_list = ['p','r','o','b','e']
+
+# # last item
+# print(my_list[-1])
+
+# # fifth last item
+# print(my_list[-5])
+
+# my_list = ['p','r','o','g','r','a','m','i','z']
+
+# print(my_list[1:3])
+# print(my_list[:-3])
+# print(my_list[:])
+
+odd = [2, 4, 6, 8]
+
+# change the 1st item    
+odd[0] = 1            
+
+print(odd)
+
+# change 2nd to 4th items
+odd[1:4] = [3, 5, 7]  
+
+print(odd) 
